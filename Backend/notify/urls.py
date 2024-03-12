fdc7a9a5902d18db4e2879908a85249fa5cabfee
Azeem Idrisi <azeemidrisi@protonmail.com>
2024-03-12 22:13:52 +0530

    Added JS Video and Python Streaming

--- Backend/notify/urls.py
+++ Backend/notify/urls.py
@@ -3,4 +3,5 @@ from django.urls import path
 
 urlpatterns = [
     path("", views.homepage, name="homepage"),
+    # path("video_feed/", views.video_feed, name="video_feed"),
 ]
--- Backend/notify/views.py
+++ Backend/notify/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, redirect
+from django.http import StreamingHttpResponse
 import pywhatkit
+from . import streaming
 
 number = "+91xx"
 
@@ -13,4 +15,15 @@ def homepage(request):
         print()
         pywhatkit.sendwhatmsg_instantly(number, message, wait_time=10)
         return redirect("/")
+
     return render(request, "notify/index.html")
+
+
+def video_feed(request):
+    cam = streaming.VideoCamera()
+    try:
+        return StreamingHttpResponse(
+            streaming.gen(cam), content_type="multipart/x-mixed-replace;boundary=frame"
+        )
+    except:
+        print("Some Error Occurred")
