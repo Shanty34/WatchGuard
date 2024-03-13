72677da82edbd0bc5ea47c7bb1beed3d5d9b0643
Azeem Idrisi <azeemidrisi@protonmail.com>
2024-03-13 13:55:26 +0530

    Live video final

--- Backend/notify/urls.py
+++ Backend/notify/urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 
 urlpatterns = [
     path("", views.homepage, name="homepage"),
-    # path("video_feed/", views.video_feed, name="video_feed"),
+    path("video_feed/", views.video_feed, name="video_feed"),
 ]
