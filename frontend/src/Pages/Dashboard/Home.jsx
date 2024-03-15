22af21da80b4d7ab97ebc2c906010b1205b7823f
MridulTi <mridultiwari2002@gmail.com>
2024-03-15 16:13:05 +0530

    Dashboard Fixed

--- frontend/src/Pages/Dashboard/Home.jsx
+++ frontend/src/Pages/Dashboard/Home.jsx
@@ -1,18 +1,49 @@
 import React from 'react'
 import { AspectRatio } from '../../components/ui/aspect-ratio'
 import { useDashboardContext } from './DashboardContext';
+import { HoverCard, HoverCardContent, HoverCardTrigger } from '../../components/ui/hover-card';
 
 
 
 export default function Home() {
     const { isComponentVisible, toggleComponentVisibility } = useDashboardContext();
     return (
-        <div className='grid place-items-center w-96'>
-            
-            {isComponentVisible&&<div className='aspect-video bg-gray-6 p-52 w-332'>
+        <div className='grid grid-cols-3 place-items-center w-[75vw] h-full'>
 
-            </div>}
-                   
+            {isComponentVisible &&
+            <HoverCard>
+                <HoverCardTrigger>
+                    <div className='aspect-square bg-gray-6 p-52 w-332'>
+                    </div>
+                    <HoverCardContent>
+                        CAM_1
+                    </HoverCardContent>
+                </HoverCardTrigger>
+            </HoverCard>
+            }
+            {isComponentVisible &&
+            <HoverCard>
+                <HoverCardTrigger>
+                    <div className='aspect-square bg-gray-6 p-52 w-332'>
+                    </div>
+                    <HoverCardContent>
+                        CAM_2
+                    </HoverCardContent>
+                </HoverCardTrigger>
+            </HoverCard>
+            }
+            {isComponentVisible &&
+            <HoverCard>
+                <HoverCardTrigger>
+                    <div className='aspect-square bg-gray-6 p-52 w-332'>
+                    </div>
+                    <HoverCardContent>
+                        CAM_3
+                    </HoverCardContent>
+                </HoverCardTrigger>
+            </HoverCard>
+            }
+
 
         </div>
     )
